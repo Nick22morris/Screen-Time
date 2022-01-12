1333cab56f87829d68b1ef6453f4954108fa51b5
Nick22morris <nick88morris22@gmail.com>
2022-01-11 23:25:59 -0600

    Initial Commit

---
+++ Screen Time/Screen_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_TimeApp.swift
+//  Screen Time
+//
+//  Created by Other user on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Screen_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
